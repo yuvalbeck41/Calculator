1fe461d149bac45af21121ac7bfcb9c61b9d979c
Barr Kirel <barr.kirel.41@gmail.com>
2021-04-11 15:33:17 +0300

    Added addition documentation

--- operators/addition.py
+++ operators/addition.py
@@ -3,6 +3,9 @@ from operators import operator
 
 @operator.Operator.register_operator
 class Addition(operator.Operator):
+    """
+        The addition operator, this operator allows us to add two numbers together
+    """
     def __init__(self):
         super().__init__('+')
 
