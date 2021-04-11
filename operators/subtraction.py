5361793c8fb0bb3d6e00534c0a73e703c49e4c50
Barr Kirel <barr.kirel.41@gmail.com>
2021-04-11 15:40:52 +0300

    Fixed the subtraction sign

--- operators/subtraction.py
+++ operators/subtraction.py
@@ -7,7 +7,7 @@ class Subtraction(operator.Operator):
         The subtraction operator, this operator allows us to subtraction one number from the other
     """
     def __init__(self):
-        super().__init__('+')
+        super().__init__('-')
 
     def act(self, x: int, y: int) -> int:
         return x - y
