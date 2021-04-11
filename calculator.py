8632d42b586ce2343022a0f41fe684972e1f0eaf
Barr Kirel <barr.kirel.41@gmail.com>
2021-04-11 15:33:08 +0300

    Added calculator documentation

--- calculator.py
+++ calculator.py
@@ -22,12 +22,17 @@ class Calculator:
         self.operators = Operator.get_registered_operators()
 
     def run(self):
+        """
+        This method runs the calculator, and presents the UI to the user
+        """
         print("*" * 40)
         print("* Welcome to our super calculator")
         print("* In order to exit type exit")
         print("* The currently supported operations:")
+        # Here we print all of the supported operators
         for operat in self.operators:
             print("*", operat.get_symbol())
+        # If we don't have any operators we wish to exit
         if len(self.operators) == 0:
             print("* We don't support any operators yet")
             exit()
@@ -36,10 +41,12 @@ class Calculator:
             inp = input(PADDING + " ")
             if inp == "exit":
                 break
+            # We get our input and then we match it to the regex expression
             group = patt.match(inp)
             if group is None:
                 print(PADDING, "Please follow the syntax: <number><operation><number>")
                 continue
+            # We check to see if the operator we were given is supported
             operator = group.group(2)
             operation = None
             for operat in self.operators:
@@ -51,6 +58,7 @@ class Calculator:
                 continue
             x = int(group.group(1))
             y = int(group.group(3))
+            # If  it is supported we will print out the result
             print(PADDING, operation.act(x, y))
         print("Good bye")
 
